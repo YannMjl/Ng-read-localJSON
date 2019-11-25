992dd0e2a17a34e1e380d9713076bf360723dbdb
Yann Mulonda <xst-mulo7954@stthomas.edu>
2019-11-25 16:10:10 -0600

    added pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import demodata from '../assets/demo.json';
 
+import { FileSizePipe } from './shared/size.pipe';
+import { CapitalizePipe } from './shared/capitalize.pipe';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    providers: [FileSizePipe, CapitalizePipe]
 })
 export class AppComponent implements OnInit {
 
-    title = 'read-json-demo';
+    title = 'How to use pipe in Angular Demo';
     Players: any = demodata;
 
-    public map: any = { lat: 51.678418, lng: 7.809007 };
-    public chart1Type: string = 'bar';
-    public chart2Type: string = 'pie';
-    public chart3Type: string = 'line';
-    public chart4Type: string = 'radar';
-    public chart5Type: string = 'doughnut';
+    data1 = [50, 40, 60, 51, 56, 55, 40];
+    data2 = [28, 80, 40, 69, 36, 37, 110];
+    data3 = [38, 58, 30, 90, 45, 65, 30];
 
-
+    // public map: any = { lat: 51.678418, lng: 7.809007 };
+    public chart1Type = 'bar';
     public chartType = 'line';
 
-    public chartDatasets: Array<any> = [
-        { data: [50, 40, 60, 51, 56, 55, 40], label: '#1' },
-        { data: [28, 80, 40, 69, 36, 37, 110], label: '#2' },
-        { data: [38, 58, 30, 90, 45, 65, 30], label: '#3' }
-    ];
+    public chartDatasets: Array<any>;
 
     public chartLabels: Array<any> = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'];
 
@@ -59,8 +57,21 @@ export class AppComponent implements OnInit {
         }
     };
 
-    constructor() {}
+    constructor(
+        private capitalize: CapitalizePipe,
+        private datasize: FileSizePipe
+    ) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+
+        // this.data1 = this.capitalize.transform(this.data1);
+
+        this.chartDatasets = [
+            { data: [50, 40, 60, 51, 56, 55, 40], label: '#1' },
+            { data: [28, 80, 40, 69, 36, 37, 110], label: '#2' },
+            { data: [38, 58, 30, 90, 45, 65, 30], label: '#3' }
+        ];
+
+    }
 
 }
