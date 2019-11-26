bda1f741a8a1640a613957db2d2fcea4a308e0c0
Yann Mulonda <xst-mulo7954@stthomas.edu>
2019-11-25 18:32:05 -0600

    added filter pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,39 @@ import demodata from '../assets/demo.json';
 
 import { FileSizePipe } from './shared/size.pipe';
 import { CapitalizePipe } from './shared/capitalize.pipe';
+import { OrderByPipe } from './shared/orderBy.pipe.js';
+import { FilterPipe } from './shared/filter.pipe.js';
+import { MycurrencyPipe } from './shared/mycurrency.pipe.js';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    providers: [FileSizePipe, CapitalizePipe]
+    providers: [
+        FileSizePipe,
+        CapitalizePipe,
+        OrderByPipe,
+        FilterPipe
+    ]
 })
 export class AppComponent implements OnInit {
 
-    title = 'How to use pipe in Angular Demo';
+    title = 'how to use pipe in angular demo';
     Players: any = demodata;
 
-    data1 = [50, 40, 60, 51, 56, 55, 40];
-    data2 = [28, 80, 40, 69, 36, 37, 110];
-    data3 = [38, 58, 30, 90, 45, 65, 30];
+    Names = [
+        'Lionel Messi',
+        'Sergio Ramos',
+        'Paul Pogba',
+        'Steven Gerrard',
+        'Christian Ericksen',
+        'Dele Ali',
+        'Luis Suárez',
+        'Kylian Mbappé',
+        'Kevin De Bruyne',
+        'Gerard Piqué',
+        'Mohamed Salah'
+    ];
 
     // public map: any = { lat: 51.678418, lng: 7.809007 };
     public chart1Type = 'bar';
@@ -36,40 +54,74 @@ export class AppComponent implements OnInit {
     public showOnlyOptionsSelect: any[];
     public filterOptionsSelect: any[];
 
-    public chartOptions: any = {
-        responsive: true,
-        legend: {
-            labels: {
-                fontColor: '#5b5f62',
-            }
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    fontColor: '#5b5f62',
-                }
-            }],
-            xAxes: [{
-                ticks: {
-                    fontColor: '#5b5f62',
-                }
-            }]
-        }
-    };
+    public chartOptions: any;
 
     constructor(
         private capitalize: CapitalizePipe,
-        private datasize: FileSizePipe
+        private datasize: FileSizePipe,
+        // private curency: MycurrencyPipe
     ) {}
 
     ngOnInit() {
 
-        // this.data1 = this.capitalize.transform(this.data1);
+        // let use capitlaize pipe on the title
+        this.title = this.capitalize.transform(this.title);
+
+        // // tslint:disable-next-line: forin
+        // for (let value in this.data1) {
+
+        //     let new_value = this.data1[value];
+        //     new_value =
+        //     new_value = this.datasize.transform(new_value)
+
+        //     console.log(new_value);  // output: Apple Orange Banana
+        // }
+
+
+        this.chartOptions = {
+            responsive: true,
+
+            legend: {
+                labels: {
+                    fontColor: '#5b5f62',
+                }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        fontColor: '#5b5f62',
+                        // tslint:disable-next-line: object-literal-shorthand
+                        // tslint:disable-next-line: only-arrow-functions
+                        userCallback(tick) {
+                            return tick.toString() + 'dB';
+                            // return this.curency.transform(tick);
+                        }
+                    },
+                    scaleLabel: {
+                        labelString: 'Amount of Money',
+                        display: true,
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                        fontColor: '#5b5f62',
+                    },
+                    scaleLabel: {
+                        labelString: 'Month',
+                        display: true,
+                    }
+                }]
+            }
+        };
+
 
         this.chartDatasets = [
-            { data: [50, 40, 60, 51, 56, 55, 40], label: '#1' },
-            { data: [28, 80, 40, 69, 36, 37, 110], label: '#2' },
-            { data: [38, 58, 30, 90, 45, 65, 30], label: '#3' }
+            {
+                data: [50, 40, 60, 51, 56, 55, 40],
+                label: 'Yann'
+            },
+            { data: [28, 80, 40, 69, 36, 37, 110], label: 'Maria' },
+            { data: [38, 58, 30, 90, 45, 65, 30], label: 'Gaby' }
         ];
 
     }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,22 @@ import { AppComponent } from './app.component';
 import { OrderByPipe } from './shared/orderBy.pipe';
 import { FileSizePipe } from './shared/size.pipe';
 import { CapitalizePipe } from './shared/capitalize.pipe';
+import { MycurrencyPipe } from './shared/mycurrency.pipe';
+import { FilterPipe } from './shared/filter.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrderByPipe,
     FileSizePipe,
-    CapitalizePipe
+    CapitalizePipe,
+    MycurrencyPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
